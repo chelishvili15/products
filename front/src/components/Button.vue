ae54d386ecfd475e13d9af6639dd621bc4c6dbf9
chelishvili15 <d.chelishvili91@gmail.com>
2024-03-15 15:19:50 +0400

    feat: button component

---
+++ front/src/components/Button.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-blue-700 text-white inline py-4 px-7 text-xl rounded-lg cursor-pointer hover:bg-blue-800 h-min text-center">
+        <slot />
+    </div>
+</template>
--- front/src/views/Product.vue
+++ front/src/views/Product.vue
@@ -1,22 +1,28 @@
 <template>
-    <div>
+    <div class="flex justify-around">
         <div class="w-1/2 m-4 border-2 border-gray-300 rounded-xl">
             
             <Carousel :images="product?.images" />
 
             <div class="text-gray-800 mt-10 flex flex-col gap-2">
-                <div v-for="title in titles" :key="title" class="flex items-end gap-3">
+                <div v-for="title in titles" :key="title" class="flex items-start gap-3">
                     <p class="text-lg font-semibold ">{{ title }}: </p>
                     <p>{{ product && product[title] }}</p>
                 </div>
             </div>
 
         </div>
+
+        <div class="py-10 flex flex-col gap-4">
+            <Button>Buy now</Button>
+            <Button>Add to basket</Button>
+        </div>
     </div>
 </template>
 
 <script setup>
 import Carousel from '../components/Carousel.vue'
+import Button from '../components/Button.vue'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import axios from 'axios'
